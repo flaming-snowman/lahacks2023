dd8a412a040c03f66f6a993640613d4a0dee3f40
Kevin1729 <kxia1729@gmail.com>
2023-04-22 17:24:49 -0700

    What

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 function DuckButton({}) {
-  return (
-    <div id="duck-btn">
-      <button class="btn">sksksk</button>
-    </div>
-  );
+    return (
+        <div id="duck-btn">
+            <button class="btn">sksksk</button>
+        </div>
+    );
 }
 function GooseButton({}) {
-  return <button>GOOSE</button>;
+    return <button>GOOSE</button>;
 }
 export default function Board() {
-  return (
-    <>
-      <DuckButton />
-      <GooseButton />
-    </>
-  );
+    return (
+        <>
+            <DuckButton />
+            <GooseButton />
+        </>
+    );
 }

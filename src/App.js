121e220a31f1cbe0b39846ccd72df2ee151a96b9
Kevin1729 <kxia1729@gmail.com>
2023-04-22 18:13:36 -0700

    Add progress bar

--- src/App.js
+++ src/App.js
@@ -1,49 +1,77 @@
-import duck from './img/duck.png';
-import goose from './img/goose.png';
-import { useState } from 'react';
-import './App.css'
+import duck from "./img/duck.png";
+import goose from "./img/goose.png";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import "./App.css";
 
 function ImageButton(props) {
-  return (
-    <img
-        width={300}
-        height={300}
-        src={props.imageSrc}
-        title={props.imageTitle}
-        onClick={props.onClick}
-    />
-  );
+    return (
+        <img
+            width={300}
+            height={300}
+            src={props.imageSrc}
+            title={props.imageTitle}
+            onClick={props.onClick}
+            alt="bird"
+        />
+    );
 }
 
-
-function CounterButton({src, text}) {
+function CounterButton({ src, text, updateKarma, add }) {
     const [count, setCount] = useState(0);
     function gotClicked() {
-        setCount(count+1);
+        setCount(count + 1);
+        updateKarma(add);
     }
 
-    return (<><ImageButton
+    return (
+        <div
+            style={{
+                margin: "100px",
+                border: "10px solid black",
+                textAlign: "center",
+            }}
+        >
+            <ImageButton
                 imageSrc={src}
                 imageTitle={count}
                 onClick={gotClicked}
             />
-            <h1>You clicked the {text} {count} times</h1></>
+            <h1>
+                You clicked the {text} {count} times
+            </h1>
+        </div>
     );
 }
 
-function DuckButton({}) {
-    return <CounterButton src={duck} text="duck"/>;
+function DuckButton({ func }) {
+    return <CounterButton src={duck} updateKarma={func} add={1} text="duck" />;
 }
 
-function GooseButton({}) {
-    return <CounterButton src={goose} text="goose"/>;
+function GooseButton({ func }) {
+    return (
+        <CounterButton src={goose} updateKarma={func} add={-1} text="goose" />
+    );
 }
 
 export default function Board() {
-  return (
-    <>
-      <DuckButton />
-      <GooseButton />
-    </>
-  );
+    const [karma, setKarma] = useState(0);
+    function updateKarma(add) {
+        setKarma(karma + add);
+        if (karma + add < 0) setKarma(0);
+        if (karma + add > 20) setKarma(20);
+    }
+    return (
+        <>
+            <div style={{ padding: "20px" }}>
+                <ProgressBar now={karma * 5} />
+            </div>
+            <h1>Karma: {karma}</h1>
+            <div style={{ display: "flex" }}>
+                <DuckButton func={updateKarma} />
+                <GooseButton func={updateKarma} />
+            </div>
+        </>
+    );
 }
